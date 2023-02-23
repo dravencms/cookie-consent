cd6f8361952d11097209f0c3c7a7b84489e53ef9
Adam Schubert <adam.schubert@sg1-game.net>
2023-02-23 04:11:38 +0100

    feat(Button): Do not display button when no settings is provided

--- src/Dravencms/FrontModule/Components/CookieConsent/CookieConsent/SettingsButton/SettingsButton.php
+++ src/Dravencms/FrontModule/Components/CookieConsent/CookieConsent/SettingsButton/SettingsButton.php
@@ -2,6 +2,7 @@
 namespace Dravencms\FrontModule\Components\CookieConsent\CookieConsent\SettingsButton;
 
 use Dravencms\Components\BaseControl\BaseControl;
+use Dravencms\Model\CookieConsent\Repository\SettingsRepository;
 
 /**
  * Class Tracking
@@ -9,9 +10,19 @@ use Dravencms\Components\BaseControl\BaseControl;
  */
 class SettingsButton extends BaseControl
 {
+    /** @var SettingsRepository */
+    public $settingsRepository;
+
+    public function __construct(SettingsRepository $settingsRepository) 
+    {
+        $this->settingsRepository = $settingsRepository;
+    }
+
     public function render(): void
     {
+        $settings = $this->settingsRepository->getOneByActive();
         $template = $this->template;
+        $template->settings = $settings;
         $template->setFile(__DIR__.'/button.latte');
         $template->render();
     }
